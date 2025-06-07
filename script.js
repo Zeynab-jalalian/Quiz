2141a392059a09582c5dad5b1b587d9109a5b16a
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-07 11:22:35 +0330

    js part2

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let quizCategory="Art";
+let quizCategory = "Art";
 
-const getRandomQuestion=()=>{
-    const categoryQuestions=questions.find(cat=>cat.category.toLowerCase() === quizCategory.toLowerCase()).questions; //access to all questions of one ctegory
+const getRandomQuestion = () => {
+    const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || []; //access to all questions of one ctegory
     const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)]; //one random question of that
-    console.log(randomQuestion);    
+    return randomQuestion;
 }
 
-getRandomQuestion();
+const randomQuestion = () => {
+    const currentQuestion = getRandomQuestion();
+    if (!currentQuestion) return;
+
+    document.querySelector(".question-text").textContent = currentQuestion.question;
+}
+
+randomQuestion();
