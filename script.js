47c6cdd4d5b4802a7e3bf655c813e4faa5beb5b7
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-07 11:40:00 +0330

    js part3

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let quizCategory = "Art";
 
-const getRandomQuestion = () => {
+const getRandomQuestion = () => { //choose a random question
     const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || []; //access to all questions of one ctegory
     const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)]; //one random question of that
     return randomQuestion;
 }
 
-const randomQuestion = () => {
-    const currentQuestion = getRandomQuestion();
-    if (!currentQuestion) return;
+const showRandomQuestion = () => { //show that random question
+    const currentQuestion = getRandomQuestion(); //current questuion is getRandomQuestion();
+    if (!currentQuestion) return; // if no question do not go ahead
 
-    document.querySelector(".question-text").textContent = currentQuestion.question;
+    document.querySelector(".question-text").textContent = currentQuestion.question; //show question in question-text
 }
 
-randomQuestion();
+showRandomQuestion();
